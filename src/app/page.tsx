db3cf920f67ec6d204cfd9cfe9e314af33a7fc07
khaleelSP2013 <mahalthykhaleel@gmail.com>
2024-07-30 08:19:01 +0530

    new  Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,12 +35,7 @@ export default function Home() {
   </div>
   <div
     className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
-    <div className="rounded-t-lg h-32 overflow-hidden">
-        <img className="object-cover object-top w-full" src='https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Mountain'>
-    </div>
-    <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
-        <img className="object-cover object-center h-32" src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Woman looking front'>
-    </div>
+   
     <div className="text-center mt-2">
         <h2 className="font-semibold">Sarah Smith</h2>
         <p className="text-gray-500">Freelance Web Designer</p>
