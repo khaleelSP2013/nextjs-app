22f1d610b263882b640f82fadef7fe73b42d1f65
khaleelSP2013 <mahalthykhaleel@gmail.com>
2024-07-30 08:56:21 +0530

    resumeupdate  Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,23 +11,25 @@ export default function Home() {
             <div className="col-span-4 sm:col-span-3">
                 <div className="bg-white shadow rounded-lg p-6">
                     <div className="flex flex-col items-center">
-                        <img src="https://randomuser.me/api/portraits/men/94.jpg" className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6C5PH28b9xIaMZmDTVb_J0ap2ffRQQKx45w&s" className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
 
                         </img>
-                        <h1 className="text-xl font-bold">John Doe</h1>
-                        <p className="text-gray-700">Software Developer</p>
-                        <div className="mt-6 flex flex-wrap gap-4 justify-center">
+                        <h1 className="text-xl font-bold">Mahalthy Khaleel Ahmed</h1>
+                        <p className="text-gray-700">Lead Software Engineer</p>
+                        <p className="text-gray-500">+91 8296485050</p>
+                        <p className="text-gray-500">mahalthykhaleel@hotmail.com</p>
+                        {/* <div className="mt-6 flex flex-wrap gap-4 justify-center">
                             <a href="#" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact</a>
                             <a href="#" className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded">Resume</a>
-                        </div>
+                        </div> */}
                     </div>
                     <hr className="my-6 border-t border-gray-300" />
                     <div className="flex flex-col">
                         <span className="text-gray-700 uppercase font-bold tracking-wider mb-2">Skills</span>
                         <ul>
-                            <li className="mb-2">JavaScript</li>
+                            <li className="mb-2">Net Core</li>
                             <li className="mb-2">React</li>
-                            <li className="mb-2">Node.js</li>
+                            <li className="mb-2">Next.js</li>
                             <li className="mb-2">HTML/CSS</li>
                             <li className="mb-2">Tailwind Css</li>
                         </ul>
@@ -37,11 +39,7 @@ export default function Home() {
             <div className="col-span-4 sm:col-span-9">
                 <div className="bg-white shadow rounded-lg p-6">
                     <h2 className="text-xl font-bold mb-4">About Me</h2>
-                    <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est
-                        vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas
-                        suscipit. Nunc finibus vel ante id euismod. Vestibulum ante ipsum primis in faucibus orci luctus
-                        et ultrices posuere cubilia Curae; Aliquam erat volutpat. Nulla vulputate pharetra tellus, in
-                        luctus risus rhoncus id.
+                    <p className="text-gray-700">12+ years of experience in Software Development, Deployment & Support and Project Execution. Efficient in merging end user needs with business requirements, budgetary restrictions, and logical considerations to meet project deliverables. Domain Specialized in: Financial Institutions, Education, Payroll & HR, Warehouse & Procurement. Detail-oriented and results-driven Full Stack Developer with 2 years of experience, specializing in build processes (CI/CD and Jenkins). Seeking to contribute expertise in both business analysis and full stack development to a dynamic team, leveraging hands-on experience to drive innovation and efficiency.
                     </p>
 
                     <h3 className="font-semibold text-center mt-3 -mb-2">
@@ -94,10 +92,10 @@ export default function Home() {
                     <h2 className="text-xl font-bold mt-6 mb-4">Experience</h2>
                     <div className="mb-6">
                         <div className="flex justify-between flex-wrap gap-2 w-full">
-                            <span className="text-gray-700 font-bold">Web Developer</span>
+                            <span className="text-gray-700 font-bold">Lead Software Engineer</span>
                             <p>
-                                <span className="text-gray-700 mr-2">at ABC Company</span>
-                                <span className="text-gray-700">2017 - 2019</span>
+                                <span className="text-gray-700 mr-2">at TAO Digital Solutions Limited </span>
+                                <span className="text-gray-700">2024 - till date</span>
                             </p>
                         </div>
                         <p className="mt-2">
@@ -108,10 +106,10 @@ export default function Home() {
                     </div>
                     <div className="mb-6">
                         <div className="flex justify-between flex-wrap gap-2 w-full">
-                            <span className="text-gray-700 font-bold">Web Developer</span>
+                            <span className="text-gray-700 font-bold">	Principal Engineer II </span>
                             <p>
-                                <span className="text-gray-700 mr-2">at ABC Company</span>
-                                <span className="text-gray-700">2017 - 2019</span>
+                                <span className="text-gray-700 mr-2">at Bhavna Corp. (Bhavna Software India Pvt. Ltd.)</span>
+                                <span className="text-gray-700">2021 - 2024</span>
                             </p>
                         </div>
                         <p className="mt-2">
@@ -122,10 +120,10 @@ export default function Home() {
                     </div>
                     <div className="mb-6">
                         <div className="flex justify-between flex-wrap gap-2 w-full">
-                            <span className="text-gray-700 font-bold">Web Developer</span>
+                            <span className="text-gray-700 font-bold">	Sr. Software Engineer </span>
                             <p>
-                                <span className="text-gray-700 mr-2">at ABC Company</span>
-                                <span className="text-gray-700">2017 - 2019</span>
+                                <span className="text-gray-700 mr-2">at Delta Technology and Management Services Private Ltd</span>
+                                <span className="text-gray-700">2019 - 2021</span>
                             </p>
                         </div>
                         <p className="mt-2">
